a5b8d8b09fd7395dbfb6de79249bb5e84b3490d8
harshit <harshit.it.21@nitj.ac.in>
2021-12-23 02:25:56 +0530

    PostActivity

---
+++ app/src/main/java/com/example/railwayfood/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.railwayfood
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+class SplashActivity : AppCompatActivity() {
+    private val splashScreen =4000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+     startActivity(intent)
+val intent = Intent( this@SplashActivity, LogIn::class.java)
+       startActivity(intent)
+       finish()
+   },splashScreen.toLong())
+
+
+
+    }
+}
